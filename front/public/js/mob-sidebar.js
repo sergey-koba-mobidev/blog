3e1b3862a44074461e40d8f43f5555d994bd36cc
Sergey Koba <s.koba@mobidev.biz>
2017-03-14 23:00:45 +0200

    fix mobile sidebar

--- front/public/js/mob-sidebar.js
+++ front/public/js/mob-sidebar.js
@@ -1,28 +1,10 @@
-var sidebar = {
-
-    $body: $('body'),
-
-    init: function () {
-        var self = this;
-        
-        self.bindEvents();
-    },
-    bindEvents: function () {
-        var self = this;
-        
-        self.handle();
-    },
-    handle: function () {
-        var self = this;
-        
-        $('.j-sidebar-toggle').on('click', function () {
-            if (self.$body.hasClass('active-mob-sidebar')) {
-                self.$body.removeClass('active-mob-sidebar');
-            } else {
-                self.$body.addClass('active-mob-sidebar');
-            }
-        })
-    }
-};
-
-sidebar.init();
+$(function () {
+    $('.j-sidebar-toggle').on('click', function () {
+        var body = $('body');
+        if (body.hasClass('active-mob-sidebar')) {
+            body.removeClass('active-mob-sidebar');
+        } else {
+            body.addClass('active-mob-sidebar');
+        }
+    })
+});
