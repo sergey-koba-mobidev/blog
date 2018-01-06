e6ef9aad6ccb0722a00509d1e096dcaca71f308b
Serge Koba <s.koba@mobidev.biz>
2018-01-06 23:42:32 +0200

    add line numbers and blockchain

--- front/public/highlightjs/highlight.js
+++ front/public/highlightjs/highlight.js
@@ -2,4 +2,7 @@ $(function () {
     $('article pre').each(function (i, block) {
         hljs.highlightBlock(block);
     });
+    $('article pre.hljs').each(function(i, block) {
+        hljs.lineNumbersBlock(block);
+    });
 });
